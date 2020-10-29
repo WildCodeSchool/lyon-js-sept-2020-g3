d0fa169766071a759487cced86a07d9e3c3effb3
Guillem <guillem.dardill@gmail.com>
2020-10-29 15:29:43 +0100

    Ajout de la navbar - quasi fonctionnelle, quelques modifs à faire dessus

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,46 +8,86 @@ import Question from './Question';
 import Store from './Store';
 import Settings from './Settings';
 import './Navbar.scss';
+import { slide as Menu } from 'react-burger-menu';
 
-function Navbar() {
-  return (
-    <Router>
-      <div className='navbar'>
-        <h1 className='navbarLogo'>
-          <img src={require('../Images/Logo.png')} alt='logo'></img>
-        </h1>
-        <nav className='navbarLink'>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/store'>Store</Link>
-            </li>
-            <li>
-              <Link to='/settings'>Settings</Link>
-            </li>
-            <li>
-              <Link to='/contact'>Contact</Link>
-            </li>
-            <li>
-              <Link to='/connexion'>Connexion</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+// function Navbar() {
+//   return (
+//     <Router>
+//       <div className='navbar'>
+//         <h1 className='navbarLogo'>
+//           <img src={require('../Images/Logo.png')} alt='logo'></img>
+//         </h1>
+//         <div className='burgerMenu'>
+//           <div class='bar1'></div>
+//           <div class='bar2'></div>
+//           <div class='bar3'></div>
+//         </div>
+//         <nav className='navbarLink'>
+//           <ul>
+//             <li>
+//               <Link to='/'>Home</Link>
+//             </li>
+//             <li>
+//               <Link to='/store'>Store</Link>
+//             </li>
+//             <li>
+//               <Link to='/settings'>Settings</Link>
+//             </li>
+//             <li>
+//               <Link to='/contact'>Contact</Link>
+//             </li>
+//             <li>
+//               <Link to='/connexion'>Connexion</Link>
+//             </li>
+//           </ul>
+//         </nav>
+//       </div>
 
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/store' component={Store} />
-        <Route path='/settings' component={Settings} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/connexion' component={Connexion} />
-        <Route path='/question' component={Question} />
-        <Route path='/play' component={Play} />
-      </Switch>
-    </Router>
-  );
+//       <Switch>
+//         <Route exact path='/' component={Home} />
+//         <Route path='/store' component={Store} />
+//         <Route path='/settings' component={Settings} />
+//         <Route path='/contact' component={Contact} />
+//         <Route path='/connexion' component={Connexion} />
+//         <Route path='/question' component={Question} />
+//         <Route path='/play' component={Play} />
+//       </Switch>
+//     </Router>
+//   );
+// }
+
+class Navbar extends React.Component {
+  showSettings(event) {
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <Router>
+        <div className='navbar'>
+          <h1 className='navbarLogo'>
+            <img src={require('../Images/Logo.png')} alt='logo'></img>
+          </h1>
+          <Menu right width={'40%'}>
+            <Link to='/'>Home</Link>
+            <Link to='/store'>Store</Link>
+            <Link to='/settings'>Settings</Link>
+            <Link to='/contact'>Contact</Link>
+            <Link to='/connexion'>Connexion</Link>
+          </Menu>
+        </div>
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/store' component={Store} />
+          <Route path='/settings' component={Settings} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/connexion' component={Connexion} />
+          <Route path='/question' component={Question} />
+          <Route path='/play' component={Play} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default Navbar;
