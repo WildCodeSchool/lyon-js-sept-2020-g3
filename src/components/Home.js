3c791b5d69c223e5f4a5fc6108c120b3cc432ea0
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2020-10-30 10:26:25 +0100

    fix modules

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      <button className='question'>
-        <Link to='/question'>Ask me a question</Link>
+      <button type="button" className="question">
+        <Link to="/question">Ask me a question</Link>
       </button>
-      <button className='play'>
-        <Link to='/play'>Challenge me</Link>
+      <button type="button" className="play">
+        <Link to="/play">Challenge me</Link>
       </button>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
 import Connexion from './Connexion';
 import Contact from './Contact';
 import Home from './Home';
@@ -8,63 +9,63 @@ import Question from './Question';
 import Store from './Store';
 import Settings from './Settings';
 import './Navbar.scss';
-import { slide as Menu } from 'react-burger-menu';
+import logo from '../Images/Logo.png';
 
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
     this.state = { menuOpen: false };
   }
-  // showSettings(event) {
-  //   event.preventDefault();
-  // }
+
   handleStateChange = (state) => {
     this.setState({ menuOpen: state.isOpen });
   };
+
   closeMenu = () => {
     this.setState({ menuOpen: false });
   };
 
   render() {
+    const { menuOpen } = this.state;
     return (
       <Router>
-        <div className='navbar'>
-          <h1 className='navbarLogo'>
-            <img src={require('../Images/Logo.png')} alt='logo'></img>
+        <div className="navbar">
+          <h1 className="navbarLogo">
+            <img src={logo} alt="logo" />
           </h1>
           <Menu
-            isOpen={this.state.menuOpen}
+            isOpen={menuOpen}
             onStateChange={(state) => this.handleStateChange(state)}
             noOverlay
             right
-            width={'100%'}
+            width="100%"
           >
-            <Link to='/' onClick={this.closeMenu}>
+            <Link to="/" onClick={this.closeMenu}>
               Home
             </Link>
-            <Link to='/store' onClick={this.closeMenu}>
+            <Link to="/store" onClick={this.closeMenu}>
               Store
             </Link>
-            <Link to='/settings' onClick={this.closeMenu}>
+            <Link to="/settings" onClick={this.closeMenu}>
               Settings
             </Link>
-            <Link to='/contact' onClick={this.closeMenu}>
+            <Link to="/contact" onClick={this.closeMenu}>
               Contact
             </Link>
-            <Link to='/connexion' onClick={this.closeMenu}>
+            <Link to="/connexion" onClick={this.closeMenu}>
               Connexion
             </Link>
           </Menu>
         </div>
 
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/store' component={Store} />
-          <Route path='/settings' component={Settings} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/connexion' component={Connexion} />
-          <Route path='/question' component={Question} />
-          <Route path='/play' component={Play} />
+          <Route exact path="/" component={Home} />
+          <Route path="/store" component={Store} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/connexion" component={Connexion} />
+          <Route path="/question" component={Question} />
+          <Route path="/play" component={Play} />
         </Switch>
       </Router>
     );
