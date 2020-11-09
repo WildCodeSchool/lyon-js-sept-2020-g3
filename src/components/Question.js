aebb5ece08f26be75d64de10505e64858141e870
Guillem <guillem.dardill@gmail.com>
2020-11-09 18:24:41 +0100

    auto scroll to the bottom of chat mode 2 added

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
 import './Question.scss';
 import Chat from '@material-ui/icons/Chat';
-import questionImg from '../Images/archibot.png';
+import questionImg from '../Images/robot.gif';
 
 function Question() {
   const [question, setQuestion] = useState('');
@@ -12,19 +13,30 @@ function Question() {
   const [chat1, setChat1] = useState('on');
   const [chat2, setChat2] = useState('off');
 
+  // useRef utilisé pour atteindre le bas de la conversation dans le mode de chat avec tous les messages
+  const conversationBottom = useRef(null);
+  // fonction qui permettra de scroller jusqu'au useRef défini ci-dessus
+  const scrollToBottom = () => {
+    conversationBottom.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  // Mise à jour du state question au fur et à mesure qu'on l'écrit
   const updateQuestion = (e) => {
     setQuestion(e.target.value);
   };
+  // Clear de l'input de question
   const resetQuestion = () => {
     setQuestion('');
   };
+  // Mise à jour du state réponse en fonction du retour de l'API
   const updateResponse = (apiResult) => {
     setResponse(apiResult.cnt);
   };
+  // Mise à jour du state array qui rassemble toutes les questions et réponses à la suite
   const updateMessageArray = () => {
     setMessageArray([...messageArray, question, response]);
-    console.log(messageArray);
   };
+  // Envoi de la question à l'API et stockage de la réponse dans le state
   const submitToAPI = () => {
     const encodedURIMessage = encodeURIComponent(question);
     const url = `https://acobot-brainshop-ai-v1.p.rapidapi.com/get?bid=153798&key=SXUv8ChYDG1AboDK&uid=User&msg=${encodedURIMessage}`;
@@ -46,19 +58,24 @@ function Question() {
         console.log(err);
       });
   };
+  // Lance la fonction submitToAPI() sur pression de la touche entrée
   const submitQuestionWithEnter = (e) => {
     e.preventDefault();
     submitToAPI();
   };
+  // Change le state du choix de mode de conversation
   const switchChatMode = () => {
     setChat1(chat1 === 'on' ? 'off' : 'on');
     setChat2(chat2 === 'on' ? 'off' : 'on');
   };
-
+  // Met à jour le tableau de stockage des Q/A et clear l'input de question quand le state response change
   useEffect(() => {
     updateMessageArray();
     resetQuestion();
   }, [response]);
+  useEffect(() => {
+    scrollToBottom();
+  }, [messageArray]);
 
   return (
     <div>
@@ -78,19 +95,24 @@ function Question() {
         >
           {messageArray.map((element, index) => {
             return (
-              <div>
-                <div key={index} className="Message">
-                  {index !== 0 &&
-                    (index % 2 === 0 ? (
-                      <div className="humanMessage">{`${element}`}</div>
-                    ) : (
-                      <div className="botMessage">{`${element}`}</div>
-                    ))}
-                </div>
+              <div
+                /* eslint-disable */
+                key={index}
+                /* eslint-disable */
+                className="Message"
+              >
+                {index !== 0 &&
+                  (index % 2 === 0 ? (
+                    <div className="humanMessage">{`${element}`}</div>
+                  ) : (
+                    <div className="botMessage">{`${element}`}</div>
+                  ))}
               </div>
             );
           })}
+          <div ref={conversationBottom}></div>
         </div>
+
         <form
           className="writeArea"
           onSubmit={(e) => {
