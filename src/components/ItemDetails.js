a117f24008a7d7a67f52882bbacc40bac7f8c3ec
Thibault Verin <thibaultverin@hotmail.com>
2020-11-14 18:11:48 +0100

    Few CSS change

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -16,12 +16,7 @@ const ItemDetails = (props) => {
 
   const MyVerticallyCenteredModal = (props) => {
     return (
-      <Modal
-        {...props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
+      <Modal {...props}>
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
             Modal heading
