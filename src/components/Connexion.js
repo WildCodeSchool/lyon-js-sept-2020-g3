4c8bb65c680cc3a01243a291cb10c71b0fb5dcef
Thibault Verin <thibaultverin@hotmail.com>
2020-11-17 22:44:09 +0100

    set the modal on connexion page

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import './Connexion.scss';
+import Modal from './Modal';
+import './Modal.scss';
 
 const Connexion = () => {
-  return <div className="connexionBody">Connexion</div>;
+  const isModalOpen = true;
+
+  return (
+    <div className="connexionBody">
+      {' '}
+      <Modal showModal={isModalOpen}>
+        <div className="modalHeader">
+          <h2>Work In Progress ...</h2>
+        </div>
+        <div className="modalBody">
+          <h3>
+            This page is a work in progress
+            <br />
+            come check back soon !
+          </h3>
+        </div>
+        <div className="modalFooter">
+          <button type="button" className="modalButton">
+            Home
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
 };
 
 export default Connexion;
