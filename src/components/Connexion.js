59c35b0455be0a18c6eff42a7e9812ebd30c850c
Thibault Verin <thibaultverin@hotmail.com>
2020-11-17 20:31:39 +0100

    modal creation for the buy button

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Connexion.scss';
 
 const Connexion = () => {
-  return <div>Connexion</div>;
+  return <div className="connexionBody">Connexion</div>;
 };
 
 export default Connexion;
--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -2,71 +2,83 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { items } from './Store';
+import Modal from './Modal';
+import './Modal.scss';
 
 const ItemDetails = (props) => {
   const data = props.match.params;
   const { id } = data;
   const result = items.find((item) => item.id === parseInt(id, 10));
+
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const test = () => {
-    return (
-      <div>
-        <p>Hello Bitch</p>
-        <button type="button" onClick={() => setIsModalOpen(false)}>
-          Close
-        </button>
-      </div>
-    );
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
     <div>
-      {isModalOpen === false ? (
-        <div className="uniqueStore">
-          {result.id === 1 ? (
-            <Link to={`/store/${items.length}`}>
-              <div class="arrow left"></div>
+      <div className={isModalOpen ? 'uniqueStoreModalOpen' : 'uniqueStore'}>
+        {result.id === 1 ? (
+          <Link to={`/store/${items.length}`}>
+            <div class="arrow left"></div>
+          </Link>
+        ) : (
+          <Link to={`/store/${result.id - 1}`}>
+            <div class="arrow left"></div>
+          </Link>
+        )}
+        <div className="uniqueCard">
+          <img
+            className="storeImg"
+            src={result.src}
+            alt={`Avatar ${result.name}`}
+            height="150"
+            width="150"
+          />
+          <div className="uniqueStoreText">
+            <h2>{result.name}</h2>
+            <p>{result.description}</p>
+            <p className="uniqueStorePrice">{result.price}</p>
+          </div>
+          <div className="uniqueStoreButton">
+            {' '}
+            <button onClick={openModal}>Buy</button>
+            <Link to="/store">
+              <button type="button">Store</button>
+            </Link>
+          </div>
+          {result.id === items.length ? (
+            <Link to={'/store/1'}>
+              <div class="arrow right"></div>
             </Link>
           ) : (
-            <Link to={`/store/${result.id - 1}`}>
-              <div class="arrow left"></div>
+            <Link to={`/store/${result.id + 1}`}>
+              <div class="arrow right"></div>
             </Link>
           )}
-          <div className="uniqueCard">
-            <img
-              className="storeImg"
-              src={result.src}
-              alt={`Avatar ${result.name}`}
-              height="150"
-              width="150"
-            />
-            <div className="uniqueStoreText">
-              <h2>{result.name}</h2>
-              <p>{result.description}</p>
-              <p className="uniqueStorePrice">{result.price}</p>
-            </div>
-            <div className="uniqueStoreButton">
-              {' '}
-              <button onClick={() => setIsModalOpen(true)}>Buy</button>
-              <Link to="/store">
-                <button type="button">Store</button>
-              </Link>
-            </div>
-            {result.id === items.length ? (
-              <Link to={'/store/1'}>
-                <div class="arrow right"></div>
-              </Link>
-            ) : (
-              <Link to={`/store/${result.id + 1}`}>
-                <div class="arrow right"></div>
-              </Link>
-            )}
-          </div>
         </div>
-      ) : (
-        <div> {test()}</div>
-      )}
+      </div>
+
+      <Modal showModal={isModalOpen} hideModal={closeModal}>
+        <div className="modalHeader">
+          <h2>Item Not Available</h2>
+        </div>
+        <div className="modalBody">
+          <h3>
+            This item will be available very soon <br></br>don't miss it !
+          </h3>
+        </div>
+        <div className="modalFooter">
+          <button type="button" className="modalButton" onClick={closeModal}>
+            Close
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
