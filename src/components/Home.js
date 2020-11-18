f691ab903ceeb3d6efe2d445214e978b8520596f
AmandineGervis <gervis.amandine@gmail.com>
2020-11-18 16:45:24 +0100

    css and responsiv for page home and nav desktop

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.scss';
+import Background from './Background';
 
 const Home = () => {
   return (
     <div className="homeBody">
+      <Background />
       <div className="homeBubble">
         <span className="tip">Hi, I'm Archibot, your new best friend !</span>
       </div>
       <div className="imageContainer" />
       <div className="homebuttons">
         <button type="button" className="question">
-          <Link to="/question">Ask me</Link>
+          <Link to="/question">Question me</Link>
         </button>
         <button type="button" className="play">
           <Link to="/akinator">Challenge me</Link>
