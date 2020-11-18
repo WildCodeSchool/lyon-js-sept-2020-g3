b06780e6767cd756f5ea3d662557270ff28934f9
Guillem <guillem.dardill@gmail.com>
2020-11-18 16:56:38 +0100

    z-index positionning of elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,6 +42,7 @@ class Navbar extends React.Component {
           </ul>
         </div>
         <Menu
+          className="menuNavbar"
           isOpen={menuOpen}
           onStateChange={(state) => this.handleStateChange(state)}
           noOverlay
