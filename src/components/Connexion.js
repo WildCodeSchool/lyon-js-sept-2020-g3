52f5377cc9815d5a803b3f6b012095d7181e844a
Thibault Verin <thibaultverin@hotmail.com>
2020-11-18 17:16:30 +0100

    CSS on modal

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -3,25 +3,31 @@ import { Link } from 'react-router-dom';
 import './Connexion.scss';
 import Modal from './Modal';
 import './Modal.scss';
+import workInProgress from '../Images/workInProgress.png';
 
 const Connexion = () => {
   const isModalOpen = true;
 
   return (
     <div className="connexionBody">
-      {' '}
+      <h1 className="connexionTitle">Connexion</h1>
       <Modal showModal={isModalOpen}>
-        <div className="modalHeader">
+        <div className="modalHeaderConnexion">
           <h2>Work In Progress ...</h2>
         </div>
         <div className="modalBody">
+          <img
+            src={workInProgress}
+            alt="work in progress"
+            className="connexionModalImage"
+          />
           <h3>
             This page is a work in progress
             <br />
             Come check back soon !
           </h3>
         </div>
-        <div className="modalFooter">
+        <div className="modalFooterConnexion">
           <Link to="/">
             {' '}
             <button type="button" className="modalButton">
