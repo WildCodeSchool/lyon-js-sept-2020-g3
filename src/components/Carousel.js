1f255e787314416edbf1c2af3db0834b4173b4ec
Thibault Verin <thibaultverin@hotmail.com>
2020-11-24 22:38:16 +0100

    start carousel

---
+++ src/components/Carousel.js
@@ -0,0 +1,26 @@
+/* import React, { Component } from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
+const Carousel = () => {
+  return (
+    <div>
+      {' '}
+      <Carousel>
+        <div>
+          <img src="assets/1.jpeg" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src="assets/2.jpeg" />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src="assets/3.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+ */
